17cec9d7389c7f43a8fe1a8637bfc5b5939abf97
keumss <kss6609@naver.com>
2016-11-01 18:47:40 +0900

    Update index.ts

--- Server/routes/index.ts
+++ Server/routes/index.ts
@@ -1,11 +1,41 @@
-/// <reference path='../typings/tsd.d.ts' />
-
-import express = require('express');
+"use strict";
+var db = require('./db.js');
+var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+
+	var info = JSON.parse(req.headers.authoraization);
+	var sending_obj = {};
+	var isUser = false;
+
+
+	console.log(info.ID);
+	console.log(info.Password);
+
+	for(var i=0; i<db.length; i++){
+		if(info.ID==db[i].ID){
+			isUser = true;
+			break;
+		}
+	}
+
+	if(isUser){
+		sending_obj = db[i];	
+	}else{
+		db.push[{
+			ID: info.ID,
+			Password: info.ID,
+			user_coin: 0,
+			busker_coin: 0,
+			busker_heart: 0
+		}];
+		sending_obj = db[db.length-1];
+	}
+	console.log(sending_obj.ID);
+
+ 	res.send(JSON.stringify(sending_obj));
 });
 
-export = router;
+module.exports = router;
